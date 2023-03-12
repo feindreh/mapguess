b884f433c0f82a07246b2339a619e2080b2c8511
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-12 22:48:45 +0100

    add link to create

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,11 @@ import { Link } from "react-router-dom"
 function Home(){
     return(
         <div>
-            <Link to={"/play"}>
+            <Link to={"/pickPlay"}>
                 Play
+            </Link>
+            <Link to={"/create"}>
+                Create
             </Link>
         </div>
     )
