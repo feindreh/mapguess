5a093b85076681eae6736bc351bfad4283f53354
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-12 19:17:27 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css"
+import "./reset.css"
+
+import NavBar from "./components/Nav";
+import Home from "./components/Home";
 import Game from "./components/Game";
+
+
+
 
 function App() {
   return (
     <div>
-      <Game/>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/play" element = {<Game/>}/>
+      </Routes>
     </div>
   );
 }
