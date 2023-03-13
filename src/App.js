3e8a57ce663d62eec963a5a8a705b8ec691ce067
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-13 06:05:36 +0100

    add more routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./reset.css"
 import NavBar from "./components/Nav";
 import Home from "./components/Home";
 import PickPlay from "./components/PickPlay";
+import PickEdit from "./components/PickEdit";
 import Create from "./components/Create/Create";
 import Game from "./components/Game";
 
@@ -13,13 +14,16 @@ import Game from "./components/Game";
 
 
 function App() {
+
   return (
     <div>
       <NavBar/>
       <Routes>
         <Route path="/" element = {<Home/>}/>
         <Route path="/pickPlay" element = {<PickPlay/>}/>
+        <Route path="/pickEdit" element = {<PickEdit/>}/>
         <Route path="/create" element={<Create/>}/>
+        <Route path="/create/:paramid" element={<Create/>}/>
         <Route path="/play" element = {<Game/>}/>
       </Routes>
     </div>
