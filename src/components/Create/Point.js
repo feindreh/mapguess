a6bf50bc70f939d8198ef96c7d163c936f1d8fbc
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-14 01:35:00 +0100

    add linkt to editpoint

--- src/components/Create/Point.js
+++ src/components/Create/Point.js
@@ -1,8 +1,9 @@
 import { useRef } from "react"
+import { Link } from "react-router-dom"
 
 function Point(props){
 
-    const {point,deletePoint} = props
+    const {point,deletePoint,mapID} = props
     
     const name = useRef()
 
@@ -12,7 +13,9 @@ function Point(props){
                 Punkt : 
             </div>
             <input onChange={()=>{point.name=name.current.value}} ref={name}type="text" defaultValue={point.name} placeholder={"Was soll gefunden werden"}></input>
-            <button type="button">Diesen Punkt bearbeiten</button>
+            <Link to={`/editPoint/${mapID}/${point.id}`}>
+                <button type="button">Diesen Punkt bearbeiten</button>
+            </Link>
             <button type="button" onClick={()=>{deletePoint(point.id)}}>Löschen</button>
         </div>
     )
