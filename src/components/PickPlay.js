8fc0923eac9b2b64bee011d879e7643f7c957147
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-14 02:53:21 +0100

    add pickPlay functionality

--- src/components/PickPlay.js
+++ src/components/PickPlay.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react"
+import { getMapCollection } from "../firebase/firebase"
 import "./PickPlay.css"
+import { Link } from "react-router-dom"
+
+
 
 function PickPlay(){
+
+    const [maps,setMaps] = useState([])
+
+    async function load(){
+        const newMaps = await getMapCollection()
+        setMaps(newMaps)
+    }
+
+    useEffect(()=>{
+        load()
+    },[])
+
+
     return (
-        <div>PickPlay React</div>
+        <div>
+            {maps.map((map)=>{
+                return(
+                <div key={map.id}>
+                    <div>{map.name}</div>
+                    <Link to={`/play/${map.id}`}>
+                        <button type = "button">Diese Map spielen!</button>
+                    </Link>
+                </div>
+                )
+            })}
+        </div>
     )
 }
 
