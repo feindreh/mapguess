2cda30275d449d7aac823d18d4cf30fc1124ef54
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-15 04:18:04 +0100

    add description text to Links

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,30 @@ import { Link } from "react-router-dom"
 function Home(){
     return(
         <div id="home">
-            <Link className="link" to={"/pickPlay"}>
-                Play
-            </Link>
-            <Link className="link" to={"/pickEdit"}>
-                Bearbeiten
-            </Link>
-            <Link className="link" to={"/create"}>
-                Neue Karte
-            </Link>
+            <div className="descriptionWrap">
+                <Link className="link" to={"/pickPlay"}>
+                    Spielen
+                </Link>
+                <div className="description">
+                Spiele eine von der Community oder dir selbst erstellte Karte und zeig, dass du ein echter Map Guesser bist !
+                </div>
+            </div>
+            <div className="descriptionWrap">
+                <Link className="link" to={"/pickEdit"}>
+                    Bearbeiten
+                </Link>
+                <div className="description">
+                Bearbeite eine Karte, füge neue Punkte hinzu, lade ein  neues Bild hoch, oder platziere die Zielpunkte neu um deine Karte noch besser zu machen!
+                </div>
+            </div>
+            <div className="descriptionWrap">
+                <Link className="link" to={"/create"}>
+                    Neue Karte
+                </Link>
+                <div className="description">
+                 Bau dir einfach selbst ganz einfach eine neue Karte zusammen und teile sie mit der Community!
+                </div>
+            </div>
         </div>
     )
 }
