b9de5904c0f0faef2e7e44bdf9d4e6225d3bbc95
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-16 23:40:56 +0100

    add checkmark if valid

--- src/components/Create/Point.js
+++ src/components/Create/Point.js
@@ -1,15 +1,36 @@
-import { useRef } from "react"
+import { useEffect, useRef,useState } from "react"
 import { Link } from "react-router-dom"
 
 function Point(props){
 
     const {point,deletePoint,mapID,upload} = props
     
+    const [check,setCheck] = useState(false)
+
+    useEffect(()=>{
+        checkRef.current.style.position = "absolute"
+        checkRef.current.style.transform = "translate(-100%,0)"
+        checkRef.current.style.height = "calc(var(--midtext)*3)"
+        checkRef.current.style.width = "calc(var(--midtext)*3)"
+        checkRef.current.style.border = "solid black 2px"
+        checkRef.current.style.width = checkRef.current.offsetWidth
+        if(point.x!==null && point.y!==null){
+            setCheck(true)
+            checkRef.current.style.backgroundColor = "green"
+        }else{
+            setCheck(false)
+            checkRef.current.style.backgroundColor = "red"
+        }
+    }
+    ,[point.x,point.y])
+
+    const checkRef=useRef()
+
     const name = useRef()
 
     return (
         <div className="point">
-
+            <div ref={checkRef} className="check">{check?"Good":"Bad"}</div>
             <div className="inputwrap">  
                 <input className="mid content"onChange={()=>{point.name=name.current.value}} ref={name}type="text" defaultValue={point.name} placeholder={"Was soll gefunden werden"}></input>
             </div>
